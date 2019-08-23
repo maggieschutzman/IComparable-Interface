97cb48e18d7f049ede26e4ba8dafac25ac7543b0
max-student@outlook.com <max-student@outlook.com>
2019-08-23 12:12:09 -0400

    Add project files.

---
+++ InterfacesWithMike/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace InterfacesWithMike {
+    class Program {
+        static void Main(string[] args) {
+
+            Partner p1 = new Partner();
+            p1.VendorID = 123;
+            p1.CreditLimit = 1000m;
+            p1.Address = "30303 A St.";
+            PrintLabel(p1);
+
+            Partner p2 = new Partner();
+            p2.VendorID = 123;
+            p2.CreditLimit = 5000m;
+
+            //Greater than something > 0
+            if (p1.CompareTo(p2) > 0) {
+                Console.WriteLine("P1 is better");
+            }
+            else {
+                Console.WriteLine("P1 is not better");
+                 }
+            }
+
+        static void PrintLabel(Partner theAddressObject) {
+            Console.WriteLine(theAddressObject.Name + " " +theAddressObject.Address);
+            }
+        }
+    }
+
+    interface IVendor{
+
+        int VendorID { get; set; }
+        string Name { get; set; }
+        decimal CreditLimit { get; set; }
+        void YellAtVendor(string Msg);
+        string Purchase(string Item, int Qty);       
+            //Events
+        }
+
+    class Customer {
+            int CustomerId { get; set; }
+            string Name { get; set; }
+
+}
+
+
+    class Partner : IVendor, IComparable {
+        public int VendorID { get; set; }
+        public string Name { get; set; }
+        public decimal CreditLimit { get; set; }
+        public string Address { get; set; }
+
+        public int CompareTo(object obj) {
+
+            Partner theOtherGuy = (Partner)obj;
+            if (this.CreditLimit == theOtherGuy.CreditLimit) {
+                return 0; //If they're the same, return 0 always
+            } 
+            else 
+                if (this.CreditLimit > theOtherGuy.CreditLimit) {
+                    return 1;
+                }
+            
+            else {
+                return -1;
+            }
+        }
+
+        public string Purchase(string Item, int Qty) {
+            return "ok";
+        }
+
+        public void YellAtVendor(string Msg) {
+            Console.WriteLine(Msg);
+        }
+    }
+
+    class BankAccount {
+        public int fred;
+    }
+
